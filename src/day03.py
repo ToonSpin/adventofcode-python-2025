efc78d8a92d20cd89ac7ffafc60ca3ded367456a
Toon Spin <toon.spin@gmail.com>
2025-12-06 17:27:43 +0100

    Day 03: redo to greatly improve performance

--- src/day03.py
+++ src/day03.py
@@ -1,34 +1,29 @@
-import heapq
 import functools
 import sys
 
-COUNT = 12
-
-def joltage_from_iterable(t) -> int:
-    return functools.reduce(lambda a, b: a * 10 + b, t, 0)
+def joltage_from_list(t: list[int]) -> int:
+    def combine(a: int, b: int) -> int:
+        return a * 10 + b
+    return functools.reduce(combine, t, 0)
 
 def get_max_combination(l: list[int], n: int) -> list[int]:
-    queue = [tuple([[], [-i for i in l], n])]
+    if n == 1:
+        return [max(l)]
+    
+    if n == len(l):
+        return l
 
-    while len(queue) > 0:
-        partial, ll, nn = heapq.heappop(queue)
+    max_element = max(l[:-(n-1)])
+    index = l.index(max_element)
+    return [max_element] + get_max_combination(l[index+1:], n - 1)
 
-        if nn == 0:
-            return [-i for i in partial]
+total_part_one = 0
+total_part_two = 0
 
-        for i, e in enumerate(ll):
-            if nn < len(ll) - 1:
-                if e > min(ll[:-nn]):
-                    continue
-            heapq.heappush(queue, tuple([partial[:] + [e], ll[i+1:], nn - 1]))
-
-    raise Exception('queue ran out!')
-
-total = 0
-perf_comb = 0
-perf_max = 0
 for line in sys.stdin.read().strip().splitlines():
     bank = [int(c) for c in line]
-    total += joltage_from_iterable(get_max_combination(bank, COUNT))
+    total_part_one += joltage_from_list(get_max_combination(bank[:], 2))
+    total_part_two += joltage_from_list(get_max_combination(bank[:], 12))
 
-print(total)
+print(total_part_one)
+print(total_part_two)
