285631adbc8607a73e5e73c54d0f99b69d1cc60f
Toon Spin <toon.spin@gmail.com>
2025-12-07 10:54:59 +0100

    Day 07: improve legibility

--- src/day07.py
+++ src/day07.py
@@ -1,25 +1,25 @@
-import grid
+from grid import Grid, Position as Pos
 import sys
 
 
-def count_splits(manifold: grid.Grid) -> tuple[int, int]:
+def count_splits(manifold: Grid) -> tuple[int, int]:
     start = next(manifold.find('S'))
-    beams: dict[grid.Position, int] = {start: 1}
+    beams: dict[Pos, int] = {start: 1}
     y = start.y
-    splits_found: set[grid.Position] = set()
+    splits_found: set[Pos] = set()
 
     while y < manifold.height:
-        new_beams: dict[grid.Position, int] = {}
+        new_beams: dict[Pos, int] = {}
 
         for beam, count in beams.items():
-            new_positions: list[grid.Position] = []
+            new_positions: list[Pos] = []
 
             if manifold.get(beam) == '^':
                 splits_found.add(beam)
-                new_positions.append(grid.Position(beam.x - 1, y + 1))
-                new_positions.append(grid.Position(beam.x + 1, y + 1))
+                new_positions.append(Pos(beam.x - 1, y + 1))
+                new_positions.append(Pos(beam.x + 1, y + 1))
             else:
-                new_positions.append(grid.Position(beam.x, y + 1))
+                new_positions.append(Pos(beam.x, y + 1))
 
             for p in new_positions:
                 existing_count: int = new_beams.get(p, 0)
@@ -31,7 +31,7 @@ def count_splits(manifold: grid.Grid) -> tuple[int, int]:
     return len(splits_found), sum(beams.values())
 
 
-manifold = grid.Grid(sys.stdin.read().strip())
+manifold = Grid(sys.stdin.read().strip())
 
 split_count, timeline_count = count_splits(manifold)
 print(split_count)
