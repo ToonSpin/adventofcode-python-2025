5cf88e7700cb6745e78ad4595d5bb90398d57a46
Toon Spin <toon.spin@gmail.com>
2025-12-07 15:26:29 +0100

    Day 02: Redo with integers

--- src/day02.py
+++ src/day02.py
@@ -1,22 +1,57 @@
-import re
+from collections.abc import Iterable, Callable
 import sys
+
+
+type Factor = tuple[int, int, int]
+type FactorFunc = Callable[[int], Iterable[Factor]]
+
+
+def factors_part_one(max_value: int) -> Iterable[Factor]:
+    factor = 11
+    while factor <= max_value:
+        yield (factor, factor * (factor - 1) // 10, factor * (factor - 2))
+        factor = factor * 10 - 9
+
+def factors_part_two(max_value: int) -> Iterable[Factor]:
+    repdigit = 11
+    while repdigit <= max_value:
+        yield (repdigit, repdigit, repdigit * 9)
+        repdigit = repdigit * 10 + 1
+    p = 1
+    while 10 ** (p + 5) <= max_value:
+        q = 1
+        while True:
+            factor = ('1' + p * '0') * q + '1'
+            minv = ('1' + p * '0') * (q + 1)
+            maxv = ('9' * (p + 1)) * (q + 1)
+            yield(int(factor), int(minv), int(maxv))
+            q += 1
+            if int(minv) > max_value:
+                break
+        p += 1
+
+def part(max_value: int, gen_func: FactorFunc) -> int:
+    found: set[int] = set()
+
+    for start, end in input:
+        for factor, minv, maxv in gen_func(max_value):
+            minv = max(minv, start)
+            maxv = min(maxv, end)
+            for n in range(minv, maxv + 1):
+                if n % factor == 0:
+                    while n <= maxv:
+                        found.add(n)
+                        n += factor
+                        if n > factor * (factor - 2):
+                            break
+
+    return sum(found)
+
 
 raw_input = sys.stdin.read().rstrip().split(',')
 raw_split = map(lambda s: tuple(s.split('-')), raw_input)
 input: list[tuple[int, int]] = list(map(lambda t: (int(t[0]), int(t[1])), raw_split))
 
-regex_part_one = re.compile(r'^(.*)\1$')
-regex_part_two = re.compile(r'^(.*)\1+$')
-
-total_part_one = 0
-total_part_two = 0
-
-for start, end in input:
-    for n in range(start, end + 1):
-        if regex_part_one.match(str(n)) is not None:
-            total_part_one += n
-        if regex_part_two.match(str(n)) is not None:
-            total_part_two += n
-
-print(total_part_one)
-print(total_part_two)
+max_value = max(b for _, b in input)
+print(part(max_value, factors_part_one))
+print(part(max_value, factors_part_two))
